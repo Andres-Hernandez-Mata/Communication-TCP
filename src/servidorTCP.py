e4047f2ad38f5400f01ac67ce6c5e269d3b68c68
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-03-30 20:08:27 -0600

    Paso de argumentos
    Se configuran los argumentos para la ejecucion del script

--- src/servidorTCP.py
+++ src/servidorTCP.py
@@ -3,3 +3,12 @@
 import argparse
 import socket
 from cryptography.fernet import Fernet
+
+#Paso de argumentos
+description = """ Modo de uso:
+    cliente.py -msj "Mensaje a enviar"""
+
+parser = argparse.ArgumentParser(description='Port scanning', epilog=description, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-msj", metavar='MSJ', dest="msj", help="mensaje a enviar", required=True)
+params = parser.parse_args()
+
