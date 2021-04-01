17d6ef3c1a4ec7e51f1491ae41abc3b3510b5c7b
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-01 11:54:35 -0600

    Cambio en los nombre de los archivos
    Estaba mal identificado el nombre de los archivos

--- src/servidorTCP.py
+++ src/servidorTCP.py
@@ -4,42 +4,31 @@ import argparse
 import socket
 from cryptography.fernet import Fernet
 
-#Paso de argumentos
-description = """ Modo de uso:
-    cliente.py -msj "Mensaje a enviar"""
-
-parser = argparse.ArgumentParser(description='Port scanning', epilog=description, formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument("-msj", metavar='MSJ', dest="msj", help="mensaje a enviar", required=True)
-params = parser.parse_args()
-
-#Generar el objeto para cifrar
-clave = Fernet.generate_key()
-cipher_suite = Fernet(clave)
-
-#Almacenamos la clave
-file - open('clave.key', 'wb') #Se guarda en bytes wb
-file.write(clave)
-file.close()
-
-#Tomar el argumento, convertirlo a bytes
-mensaje = params.msj
-mensajeBytes = mensaje.encode()
-
-#Ciframos el mensaje
-msj_cifrado = cipher_suite.encrypt(mensajeBytes)
-print("Mensaje enviado:\n", mensaje)
-
 #Preparamos los datos de la conexion
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 2048 #Cantidad de info que se queda en memoria
+BUFFER_SIZE = 2048
 
 #Establecemos la conexion
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(msj_cifrado)
-respuesta = s.recv(BUFFER_SIZE).decode()
-s.close()
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
 
-print("Respuesta recibida: ", respuesta)
+(conn, addr) = s.accept()
+print('Direccion de conexion: ', addr)
+while True:
+    mss_cifrado = conn.recv(BUFFER_SIZE)
+    conn.send(b"Enterado. Bye!")
+    break
+conn.close()
+
+#Generar el objeto para cifrar
+file = open('clave.key','rb')
+clave = file.read()
+file.close()
+cipher_suite = Fernet(clave)
+
+mensajeBytes = cipher_suite.decrypt(mss_cifrado, None)
+mensaje = mensajeBytes.decode()
+print("Mensaje recibido:\n". mensaje)
 
